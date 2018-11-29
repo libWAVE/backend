9ac0de2bbdb07503621222a1a470cb9ee94ee0f6
Sergey Nechaev <serg.nechaev@gmail.com>
2018-11-29 21:31:15 +1100

    async config

--- src/main/java/com/libwave/backend/BackendApplication.java
+++ src/main/java/com/libwave/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package com.libwave.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableScheduling
+@EnableAsync
 public class BackendApplication {
 
 	public static void main(String[] args) {
